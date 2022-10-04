31f6687b753e72987c146cc5ac349b38589a3b12
Jorge Harbes <jharbes@hotmail.com>
2022-10-04 04:24:44 -0300

    hints

---
+++ iniciacaoCodigo.java
@@ -0,0 +1,10 @@
+import java.util.Locale;
+import java.util.Scanner;
+
+
+Locale.setDefault(Locale.US);
+
+Scanner teclado=new Scanner(System.in);
+
+
+teclado.close();
---
+++ utilitarios.java
@@ -0,0 +1,6 @@
+utilitarios java
+
+String.format("%.2f",variavel)  - colocar com duas casas decimais***
+
+
+teclado.nextLine();  -  nao pular a necessidade de entrada.
